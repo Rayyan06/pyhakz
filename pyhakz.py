a71a0a9d54f6471631dc8bbf39af2b76a2dfa0ce
Rayyan Khan <weatherwhiz06@gmail.com>
2020-04-22 21:02:01 -0400

    F

--- pyhakz.py
+++ pyhakz.py
@@ -4,33 +4,38 @@ import os
 from PIL import ImageGrab
 import numpy as np
 import cv2
-#import tensorflow as tf
+from experiment import EggFinder
 
-#q_table = np.zeros([])
-
-#MOVEMENT_SPEED = 1
-NUMBER_OF_BULLETS = 1
-
-
-EGG_PICTURE = 'egg.png'
-PLAY_BUTTON = 'PLAY_BUTTON.png'
-#PLAY_GAME = (1000, 500)
+def process_img(image):
+    processed_img = image
+    return processed_img
 
 
-def screen_record():
+    """# convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img"""
+
+def main():
     last_time = time.time()
+    eggfinder = EggFinder()
     while True:
-        # 800x600 windowed mode
-        printscreen_pil =  ImageGrab.grab(bbox=(0,40,800,640))
-
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        eggs = eggfinder.findEggs(screen)
+        print(eggs)
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        #printscreen_numpy = np.array(printscreen_pillow.getdata(),dtype='uint8').reshape((printscreen_pillow.size[1],printscreen_pillow.size[0],3))
-        #cv2.imshow('window',printscreen_numpy)
-        #if cv2.waitKey(25) & 0xFF == ord('q'):
-        #    cv2.destroyAllWindows()
-        #    break
+        new_screen = process_img(screen)
+        #egg_loc = read_image(new_screen)
+        #print(egg_loc)
+        cv2.imshow('window',screen)
+        #cv2.imshow('window', updateScreen(new_screen))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
+main()
 
-if __name__ =="__main__":
-    screen_record()
+from IPython.display import Image
+Image(filename='edge-detection.png')
